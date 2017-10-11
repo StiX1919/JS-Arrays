59e6b05741781d1baf9a2b6678beceadc7972c2a
Spencer Smith <WankFerret@Stevens-MacBook.local>
2017-10-10 23:01:28 -0500

    done

--- arraysPractice.js
+++ arraysPractice.js
@@ -276,7 +276,7 @@ of Data is to have an Array full of objects. */
 //Create an empty array called users.
 
   //Code Here
-
+var users = []
 /*Now add three user objects to your users array. Each user object should contain the
 following properties. name, email, password, username.*/
 
@@ -289,7 +289,13 @@ var user1 = {
 };
 
 //Your Code Here
-
+users.push(user1,user2={name: "",
+email: "",
+password: "",
+username: ""},user3={name: "",
+email: "",
+password: "",
+username: ""})
 /*Now you have a very common data structure. Twitter is a good use case.
 It's easy to imagine that your followers list on Twitter is an Array full or objects
 and those objects contain properties about the specific person you follow.*/
@@ -297,7 +303,7 @@ and those objects contain properties about the specific person you follow.*/
 /*Now let's say that Tyler decided to delete his account. Loop through your array of
 objects until you find Tyler's account (use tylermcginnis33@gmail.com to find him).
 Once you find the particular index he's located in, delete him from the array.*/
-
+users = users.filter(x => x.email !== 'tylermcginnis33@gmail.com')
   //Code Here
 
 //The activity we just did is very much how data works in 'the real world'.
